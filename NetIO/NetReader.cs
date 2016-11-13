42350eb5f2a723da585a32be9871b0d3969dcd3d
Dominic Houghton <technpizza@gmail.com>
2016-11-13 04:07:52 +0000

    Code commit.

---
+++ NetIO/NetReader.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace NetIO
+{
+    public class NetReader
+    {
+        private Stream s;
+
+        /* Function:        NetReader (constructor)
+         * Parameters:      Stream stream, the stream to read from
+         * Description:     Initialises this instance of the class.
+         */
+        public NetReader(Stream stream)
+        {
+            s = stream;
+            if (!s.CanRead)
+                throw new ArgumentException("Stream passed to NetReader cannot be read from");
+
+        }
+
+        /* Function:        readByte
+         * Parameters:      none
+         * Description:     Reads a single byte from the stream. If attempting to read past the end of the stream, an IOException will be thrown.
+         */
+        public byte readByte()
+        {
+            int b;
+            b = s.ReadByte();
+            if (b == -1)
+                throw new IOException("NetReader attempted to read past the end of the stream");
+            return (byte)b;
+        }
+
+        /* Function:        readByteArray
+         * Parameters:      byte[] b, the array to read the bytes into
+         *                  int offset, the offset of the array to read into
+         *                  int length, the number of bytes to read into the array
+         * Description:     Reads length bytes from the stream into the array starting at offset. If attempting to read past the end of the stream, an IOException will be thrown.
+         */
+        public void readByteArray(ref byte[] b, int offset, int length)
+        {
+            for (int i = offset; i < offset + length; i++)
+            {
+                b[i] = readByte();
+            }
+        }
+
+        /* Function:        readInt32
+         * Parameters:      none
+         * Description:     Reads a 32-bit integer from the stream in Network Byte Order. If attempting to read past the end of the stream, an IOException will be thrown.
+         */
+        public int readInt32()
+        {
+            byte[] bytes = new byte[4];
+            int retval;
+
+            readByteArray(ref bytes, 0, 4);
+
+            retval = BitConverter.ToInt32(bytes, 0);
+            retval = IPAddress.NetworkToHostOrder(retval);
+
+            return retval;
+        }
+
+        /* Function:        readString
+         * Parameters:      none
+         * Description:     Reads a UTF-8 encoded string from the stream in the format: (int32 length)(char[] UTF8chars)
+         */
+        public string readString()
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            int length;
+            length = readInt32();
+            byte[] input = new byte[length];
+            readByteArray(ref input, 0, length);
+            char[] chars = Encoding.UTF8.GetChars(input);
+            strBuilder.Append(chars);
+            return strBuilder.ToString();
+        }
+    }
+}
---
+++ NetIO/NetWriter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace NetIO
+{
+    public class NetWriter
+    {
+        private Stream s;
+
+        /* Function:        NetWriter (constructor)
+         * Parameters:      Stream stream, the stream to write to
+         * Description:     Initialises this instance of the class.
+         */
+        public NetWriter(Stream stream)
+        {
+            s = stream;
+            if (!s.CanWrite)
+                throw new ArgumentException("Stream passed to NetWriter cannot be written to");
+
+        }
+
+        /* Function:        writeByte
+         * Parameters:      byte b, the byte to write to the stream
+         * Description:     Writes a single byte, b, to the stream.
+         */
+        public void writeByte(byte b)
+        {
+            s.WriteByte(b);
+        }
+
+        /* Function:        writeByteArray
+         * Parameters:      byte[] b, the array to write from
+         *                  int offset, the offset of the array to write from
+         *                  int length, the number of bytes to write from the array
+         * Description:     Writes length bytes to the stream from the array starting at offset.
+         */
+        public void writeByteArray(ref byte[] b, int offset, int length)
+        {
+            //for (int i = offset; i < offset + length; i++)
+            //writeByte(b[i]);
+            s.Write(b, offset, length);
+        }
+
+        /* Function:        writeInt32
+         * Parameters:      int i, the integer to write to the stream
+         * Decription:      Writes a 32-bit integer to the stream in Network Byte Order.
+         */
+        public void writeInt32(int i)
+        {
+            byte[] bytes;
+            bytes = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(i));
+            writeByteArray(ref bytes, 0, 4);
+        }
+
+        /* Function:        writeString
+         * Parameters:      string s, the string to write to the stream
+         * Description:     Writes a UTF-8 encoded string to the stream in the following format: (int32 length)(char[] UTF8chars)
+         */
+        public void writeString(string s)
+        {
+            char[] chars = s.ToCharArray();
+            byte[] output = Encoding.UTF8.GetBytes(chars);
+            writeInt32(output.Length);
+            writeByteArray(ref output, 0, output.Length);
+        }
+
+        /* Function:        writeStringRaw
+         * Parameters:      string s, the string to write to the stream
+         * Description:     Writes a UTF-8 encoded string to the stream as it's passed - NO NULL TERMINATORS OR LENGTHS WRITTEN
+         */
+        public void writeStringRaw(string s)
+        {
+            char[] chars = s.ToCharArray();
+            byte[] output = Encoding.UTF8.GetBytes(chars);
+            writeByteArray(ref output, 0, output.Length);
+        }
+    }
+}
